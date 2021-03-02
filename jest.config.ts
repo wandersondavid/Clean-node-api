f5df33c49b90491902711176843d656a7550573f
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-02 01:01:36 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+
+}
