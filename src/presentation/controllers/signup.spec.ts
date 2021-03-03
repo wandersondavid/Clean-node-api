4e2089646c1e92cce4c3d4316a637dc99d8ce4b6
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-03 01:01:55 -0300

    test: esure SignUpController return an error 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,3 +15,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should returb 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpResquet = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpResquet)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
+})
