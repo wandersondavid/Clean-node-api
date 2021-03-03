eb42d38728bc08ba2527d51a573562339437aa30
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-03 00:43:57 -0300

    refactor: att script

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,12 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-
 }
