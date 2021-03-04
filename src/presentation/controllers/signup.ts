1cccb49b68686b64ea442c4f74c57f56a3bbcd58
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-03 23:33:52 -0300

    feat: ensure SignUpController return 400 if no passward and confirmarion password is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 
 export class SignUpController {
   handle (httpResquet: HttpResquet): HttpResponse {
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredFields) {
       if (!httpResquet.body[field]) {
