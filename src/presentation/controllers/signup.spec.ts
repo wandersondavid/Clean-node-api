06640380222bfce4a64416749dd245ea8db770b3
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-09 23:08:11 -0300

    refactor: nove sut creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,13 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
+
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
   test('Should returb 400 if no name is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpResquet = {
       body: {
         email: 'any_email@mail.com',
@@ -18,7 +23,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {
   test('Should returb 400 if no email is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpResquet = {
       body: {
         name: 'any_name',
@@ -34,7 +39,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {
   test('Should returb 400 if no password is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpResquet = {
       body: {
         name: 'any_name',
@@ -50,7 +55,7 @@ describe('SignUp Controller', () => {
 
 describe('SignUp Controller', () => {
   test('Should returb 400 if no password confirmation is provided', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpResquet = {
       body: {
         name: 'any_name',
