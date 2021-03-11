90012a4bc4b70b271090e1f1a6f171ca5673bf0f
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-10 23:20:37 -0300

    refactor: move all errot imports to a single file

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,6 @@
 import { SignUpController } from './signup'
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors'
 import { EmailValidator } from '../protocols/email-validator'
-
 interface SutTypes {
   sut: SignUpController,
   emailValidatorStub: EmailValidator
