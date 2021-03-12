104a01f1c90b32fcf0d0ae4ea5eea3c7904311cf
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-11 22:45:51 -0300

    feat: ensure SignUpController returs 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,7 +17,9 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-
+      if (httpResquet.body.password !== httpResquet.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
       const isValid = this.emailValidator.isValid(httpResquet.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
