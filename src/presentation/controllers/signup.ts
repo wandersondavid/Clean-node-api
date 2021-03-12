40e05b766bf58aa73766c8f0d0cafee415c4d1f6
Wanderson David <wandersondavid.ifg@gmail.com>
2021-03-11 22:49:44 -0300

    refactor: remove code duplication

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,10 +17,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httpResquet.body.password !== httpResquet.body.passwordConfirmation) {
+      const { email, password, passwordConfirmation } = httpResquet.body ;
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const isValid = this.emailValidator.isValid(httpResquet.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
