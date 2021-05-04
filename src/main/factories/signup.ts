dce6902e9501a97b91ae138cff6da706e4f74a99
Wanderson David <wandersondavid.ifg@gmail.com>
2021-05-04 00:12:43 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,15 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/acccount'
+import { SignUpController } from '../../presentation/controllers/signup /signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  return signUpController
+}
